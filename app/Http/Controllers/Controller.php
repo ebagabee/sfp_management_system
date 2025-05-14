67bb2537858f70e2bffc686ee481b95b68b89f6c
ebagabee <ebagabe.2025@gmail.com>
2025-05-14 16:36:50 -0300

    feat: create a user controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+
 abstract class Controller
 {
-    //
+    public function successResponse($payload = null, ?string $message = null, int $code = 200): JsonResponse
+    {
+        return response()->json(data: ["payload" => $payload, "message" => $message], status: $code);
+    }
+
+    public function errorResponse(?string $message = null, int $code = 404): JsonResponse
+    {
+        return response()->json(data: ["message" => $message], status: $code);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware([])->group(function () {
+    $noAuthRoutes = glob(__DIR__ . "/free-route/*.php");
+    foreach ($noAuthRoutes as $noAuthRoute) {
+        Route::group([], $noAuthRoute);
+    }
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    $authRoutes = glob(__DIR__ . "/authenticated/*.php");
+    foreach ($authRoutes as $authRoute) {
+        Route::group([], $authRoute);
+    }
+});
